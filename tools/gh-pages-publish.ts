f584fbd92be8e9bb41ad470e1643511000ac9701
Scranner <zeldas-link@hotmail.co.uk>
2021-07-26 01:21:11 +0100

    fix

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -26,6 +26,6 @@ exec('git config user.name "James McCartney"')
 exec('git config user.email "zeldas-link@hotmail.co.uk"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
-  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
+  `git push --force --quiet "https://${ghToken}@${repository}" main:gh-pages`
 )
 echo("Docs deployed!!")
